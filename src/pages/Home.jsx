bd0bf5f68bbe8ccf3e85a3d30475fc4c48f09bf4
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2024-12-18 03:46:55 +0400

    reviews section removed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -19,7 +19,7 @@ const Home = () => {
       <Skill data={skillData} data-aos="fade-right" />
       <Resume data={resumeData} />
       <PortfolioSection data={portfolioData} data-aos="fade-right" />
-      <ReviewSection data={reviewData} data-aos="fade-right" />
+      {/* <ReviewSection data={reviewData} data-aos="fade-right" /> */}
       <Contact data={contactData} socialData={socialData} data-aos="fade-right" />
     </>
   )
