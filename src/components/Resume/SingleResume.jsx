7e1f9f781a95e7f5343d74cbe0cdfff443630d39
Davit Spanderashvili <37460419+davitITguy@users.noreply.github.com>
2025-12-06 15:30:31 +0400

    added bulletpoints to experience
    added bulletpoints to experience

--- src/components/Resume/SingleResume.jsx
+++ src/components/Resume/SingleResume.jsx
@@ -1,13 +1,29 @@
 import PropTypes from 'prop-types'
 
 const SingleResume = ({ element }) => {
-  const { title, duration, subTitle, text} = element;
+  const { title, duration, subTitle, text1,text2,text3,text4} = element;
   return (
     <div className="st-resume-timeline">
       <h3 className="st-resume-timeline-title">{title}</h3>
       <div className="st-resume-timeline-duration">{duration}</div>
       <h4 className="st-resume-timeline-subtitle">{subTitle}</h4>
-      <div className="st-resume-timeline-text"><p>{text}</p></div>
+      {/* <div className="st-resume-timeline-text"><p>{text}</p></div> */}
+      <div className="st-resume-timeline-text">
+        <ul>
+          <li>
+            <p>{text1}</p>
+          </li>
+          <li>
+            <p>{text2}</p>
+          </li>
+          <li>
+            <p>{text3}</p>
+          </li>
+          <li>
+            <p>{text4}</p>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
